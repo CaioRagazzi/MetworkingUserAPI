dcd47dd0a8b75f074251f21957847771b172b462
Caio Ragazzi <ca.ragazzi@gmail.com>
2021-02-04 20:26:23 -0300

    validation

--- src/MetWorkingUserApplication/User/Validation/CreateUserRequestValidation.cs
+++ src/MetWorkingUserApplication/User/Validation/CreateUserRequestValidation.cs
@@ -1,13 +1,15 @@
+using System.Linq;
 using FluentValidation;
 using MetWorkingUserApplication.Commands;
-using MetWorkingUserApplication.Contracts.Request;
+using MetWorkingUserApplication.Interfaces;
 
 namespace MetWorkingUserApplication.Validation
 {
     public class CreateUserRequestValidation : AbstractValidator<CreateUserCommand>
     {
-        public CreateUserRequestValidation()
-        {
+        private readonly IApplicationDbContext _applicationDbContext;
+        public CreateUserRequestValidation(IApplicationDbContext _applicationDbContext)
+        {            
             RuleFor(x => x.User.Email)
                 .NotEmpty()
                 .EmailAddress();
@@ -19,6 +21,15 @@ namespace MetWorkingUserApplication.Validation
             RuleFor(x => x.User.Password)
                 .NotEmpty()
                 .MinimumLength(6);
+
+            RuleFor(x => x.User.Email)
+                .Must((createUserCommand, cancellation) => {
+                    var exists = _applicationDbContext.Users.Where(
+                        entity => entity.Email == createUserCommand.User.Email).ToList();
+
+                    return !exists.Any();
+                })
+                .WithMessage("Already exists");
         }
     }
 }
